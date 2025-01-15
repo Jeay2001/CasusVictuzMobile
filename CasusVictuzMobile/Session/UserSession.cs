c15e1bc37ba7b09765d6084030e08d0a9f747273
LGZUYD <2305329goossens@zuyd.nl>
2025-01-15 17:52:40 +0100

    UserSession opzet en Service en Repository folders aangemaakt.

---
+++ CasusVictuzMobile/Session/UserSession.cs
@@ -0,0 +1,61 @@
+﻿using CasusVictuzMobile.MVVM.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace CasusVictuzMobile.Session
+{
+    // in een ViewModel kun je dan steeds:
+    // // LoginPageViewModel:
+    // UserSession.Instance.Login("email");    
+    // // ProfileViewModel:
+    // UserSession.Instance.LoggedInUser.Name;
+    // Zonder dat je steeds 'UserSession userSession = new UserSession();' of nieuwe Service/Repository object hoeft te maken,
+    // omdat UserSession een Singleton is en UserSession.Instance een static property.
+    public class UserSession
+    {        
+        private static UserSession _instance;
+        public static UserSession Instance => _instance ??= new UserSession();
+
+        public User? LoggedInUser { get; set; }
+
+        public bool IsLoggedIn => LoggedInUser != null;
+
+        // LoadUserAsync wordt aangeroepen bij het opstarten van de app,
+        // Dus in App.xaml.cs 
+        public async Task LoadUserAsync()
+        {
+            string? loggedInUserEmail = await SecureStorage.GetAsync("loggedInUserEmail");
+
+            if (!string.IsNullOrEmpty(loggedInUserEmail))
+            {
+                // Repos/Services bestaan nog niet, 
+                // hier haalt hij dan het ingelogde User object op:
+                //
+                // UserService userService = new UserService();
+                // LoggedInUser = userService.GetUserByEmail(loggedInUserEmail);            
+            }
+        }
+
+
+        // bij het invullen van Login form, wordt deze methode aangeroepen        
+        public void Login(string userEmail)
+        {
+            Logout();
+            SecureStorage.SetAsync("loggedInUserEmail", userEmail);
+
+        }
+
+        public void Logout()
+        {
+            LoggedInUser = null;
+            SecureStorage.Remove("loggedInUserEmail");
+        }
+
+
+    }
+
+}
