e36f420b46bb771fabc314af190c77aca8a0d5ba
LGZUYD <2305329goossens@zuyd.nl>
2025-01-27 19:46:41 +0100

    Map working

--- CasusVictuzMobile/MVVM/Models/Location.cs
+++ CasusVictuzMobile/MVVM/Models/Location.cs
@@ -12,7 +12,8 @@ namespace CasusVictuzMobile.MVVM.Models
     public class Location: TableData
     {
         public string? Name { get; set; }
-        //mogelijk iets met een aps api
+        public double Latitude { get; set; } = 50.88150;        
+        public double Longitude { get; set; } = 5.95885;
 
         public void SaveOrUpdate()
         {
---
+++ CasusVictuzMobile/MVVM/ViewModel/MapViewModel.cs
@@ -0,0 +1,21 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using Microsoft.Maui.Maps;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CasusVictuzMobile.MVVM.ViewModel
+{
+    public partial class MapViewModel :ObservableObject
+    {
+        [ObservableProperty]
+        private Models.Location location;
+        public MapViewModel(int locationId)
+        {
+                     
+        }
+    }
+}
